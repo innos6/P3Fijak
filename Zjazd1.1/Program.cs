047f5414a429c993f6c19f95eca577f518345d96
Bartłomiej Fijak <bf043557@student.ath.edu.pl>
2020-09-20 11:21:00 +0200

    init

---
+++ Zjazd1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zjazd1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
