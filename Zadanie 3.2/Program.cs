d70374b5e5f4196e9d67f8c4a8b6e1458c7eb579
Bartłomiej Fijak <bf043557@student.ath.edu.pl>
2020-10-19 18:00:15 +0200

    Create Program.cs
    Zadanie 3.2

---
+++ Zadanie 3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie_3._2 {
+    class Program    {
+        public static string format(string x) {
+            string buffer="";
+            if (x.Last() != '.') {
+                x += ('.');
+            }
+            var tab = new char[x.Length];
+            tab = x.ToCharArray();
+            if ((tab[0] >= 97)&&(tab[0] <= 122)) {
+                tab[0] -= (char)32;
+                for (int i = 0; i < tab.Length; i++) {
+                    buffer+=tab[i];
+                }
+            }
+            return buffer;
+        }
+        static void Main(string[] args) {
+            string a = "ala ma kota";
+            Console.WriteLine(a);
+            Console.WriteLine(format(a));
+        }
+    }
+}
